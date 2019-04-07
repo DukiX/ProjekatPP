358636c81218d34e8976983cf4f83ff51dc25cc3
DukiX <44091654+DukiX@users.noreply.github.com>
2019-04-07 17:14:17 +0200

    07.04

--- src/rs/ac/bg/etf/pp1/Compiler.java
+++ src/rs/ac/bg/etf/pp1/Compiler.java
@@ -3,6 +3,7 @@ package rs.ac.bg.etf.pp1;
 import java_cup.runtime.*;
 import java.io.*;
 import rs.ac.bg.etf.pp1.ast.*;
+import rs.etf.pp1.mj.runtime.Code;
 import rs.etf.pp1.symboltable.*;
 
 public class Compiler {
@@ -14,11 +15,38 @@ public class Compiler {
 		Symbol s = p.parse();
 		//System.out.println(s.value.getClass());
 		Program prog = (Program)(s.value);
+		
+		//Ispis stabla:
+		//System.out.println(prog.toString(""));
+		
     	Tab.init();
 		SemanticAnalyzer stv = new SemanticAnalyzer(); 
 		prog.traverseBottomUp(stv);
+		tsdump();
+		
+		if (!p.errorDetected && stv.passed()) {
+        	File objFile = new File(args[1]);
+        	//log.info("Generating bytecode file: " + objFile.getAbsolutePath());
+        	System.out.println("Generating bytecode file: " + objFile.getAbsolutePath());
+        	if (objFile.exists())
+        		objFile.delete();
+        	
+        	// Code generation...
+        	CodeGenerator codeGenerator = new CodeGenerator();
+        	prog.traverseBottomUp(codeGenerator);
+        	Code.dataSize = stv.nVars;
+        	Code.mainPc = codeGenerator.getMainPc();
+        	Code.write(new FileOutputStream(objFile));
+        	//log.info("Parsiranje uspesno zavrseno!");
+        	System.out.println("Parsiranje uspesno zavrseno!");
+        }
+        else {
+        	//log.error("Parsiranje NIJE uspesno zavrseno!");
+        	System.out.println("Parsiranje NIJE uspesno zavrseno!");
+        }
+	}
+	
+	public static void tsdump() {
 		Tab.dump();
-		//Ispis stabla:
-		//System.out.println(prog.toString(""));
 	}
 }
